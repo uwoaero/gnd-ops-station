d55048ca89ea3ec309c334c604e5b6d0f991c8df
AlexFernandes04 <everything.alex04@gmail.com>
2024-11-05 16:52:18 -0500

    added influxdb to backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
 //import kafka library
 import { Kafka } from 'kafkajs'
 
-import express from "express"
+import express, { query } from "express"
 const app = express();
+
+import 'dotenv/config'
+
+
+import { InfluxDB, Point } from '@influxdata/influxdb-client'
+
+const bucket = "Telemetry"
+const org = "westernaerodesign"
+const url = "http://influxdb:8086"
+const influxdb = new InfluxDB({ url: url, token: process.env.INFLUXDB_TOKEN })
+const writeApi = influxdb.getWriteApi(org, bucket)
 
 //initializes kafka connection for this consumer
 const kafka = new Kafka({
@@ -26,8 +37,15 @@ const consume = async () => {
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       if (isRunning) {
+        const point = new Point('flight')
+          .tag('flight_id', id)
+          .floatField('value', parseFloat(message.value))
+          .stringField("tag", tag.toLocaleString())
+        console.log(` ${point}`)
+
+        writeApi.writePoint(point)
         console.log({
-          tag: tag,
+          tag: tag.toLocaleString(),
           value: message.value.toString(),
         })
       }
@@ -40,6 +58,7 @@ consume().catch(console.error);
 
 let isRunning = false;
 let tag = 0;
+let id = 0;
 
 app.get('/', (req, res) => {
   res.send('<h1>SEVERRRRRRRRRRRr</h1>');
@@ -59,7 +78,8 @@ app.get('/start', (req, res) => {
   }
 
   isRunning = true;
-  tag = new Date();
+  tag = new Date(Date.now() - (5 * 60 * 60 * 1000));
+  id = Math.floor(Math.random() * 10000000000000001)
 
   res.json({
     message: 'started',
@@ -100,4 +120,51 @@ app.get("/realdata", (req, res) => {
   res.json({
     dataType: dataType,
   });
+})
+
+const queryApi = influxdb.getQueryApi(org)
+
+const myQuery = async () => {
+  const fluxQuery = 'from(bucket: "Telemetry") |> range(start: 0)'
+  const flights = []
+
+  for await (const { values, tableMeta } of queryApi.iterateRows(fluxQuery)) {
+    const o = tableMeta.toObject(values)
+    // console.log(
+    //   `${o._time} ${o._measurement} for (${o.flight_id}): ${o._field}=${o._value}`
+    // )
+    flights.push(o)
+  }
+
+  return flights
+}
+
+app.get("/flights", async (req, res) => {
+  const flights = []
+
+  const query = await myQuery()
+  console.log(query)
+  for (let i = 0; i < query.length; i++) {
+    console.log(query[i].flight_id)
+    flights.push(query[i].flight_id)
+  }
+
+  res.json({ flights: flights })
+
+})
+
+app.get("/:flight_id", async (req, res) => {
+  console.log("GETTING ID")
+  console.log(req.params.flight_id)
+
+  const flights = await myQuery()
+  const selectedFlight = []
+
+  for (let i = 0; i < query.length; i++) {
+    if(req.params.flight_id == flights[i].id){
+      selectedFlight.push(flights[i])
+    }
+  }
+
+  res.json({ selectedFlight: selectedFlight })
 })
